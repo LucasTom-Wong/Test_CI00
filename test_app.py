06840ae518d9df9fcfab78fe0d269105b1c74b36
LucasTom-Wong <lgtw3450@gmail.com>
2022-04-06 08:34:05 -0400

    copypasta

--- test_app.py
+++ test_app.py
@@ -1,8 +1,17 @@
-import app as testing
+import unittest
+import app as tested_app
+import json
 
-def main():
-    app.get('/')
-    print("tested /")
 
-if __name__ = "__main__":
-    main()
+class FlaskAppTests(unittest.TestCase):
+
+    def setUp(self):
+        tested_app.app.config['TESTING'] = True
+        self.app = tested_app.app.test_client()
+
+    def test_get_hello_endpoint(self):
+        r = self.app.get('/')
+        self.assertEqual(r.data, b'Hello World!')
+
+if __name__ == '__main__':
+    unittest.main()
