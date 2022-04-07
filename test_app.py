6d514e1d746f399df16a5affe3f8ef69dc0f1f49
LucasTom-Wong <lgtw3450@gmail.com>
2022-04-07 08:12:05 -0400

    Removed certain lines and printing instead of true false

--- test_app.py
+++ test_app.py
@@ -4,14 +4,15 @@ import json
 
 
 class FlaskAppTests(unittest.TestCase):
-
+# TestCase is a subclass which creates a test case
     def setUp(self):
-        tested_app.app.config['TESTING'] = True
+        # tested_app.app.config['TESTING'] = True
         self.app = tested_app.app.test_client()
 
     def test_get_hello_endpoint(self):
         r = self.app.get('/')
-        self.assertEqual(r.data, b'Hello World!')
+        # self.assertEqual(r.data, b'Hello World!')
+        print(r)
 
 if __name__ == '__main__':
     unittest.main()
