1585e3284fa2f7e77e485eb44dcec3caedb4aa00
LucasTom-Wong <lgtw3450@gmail.com>
2022-04-08 08:34:08 -0400

    random?? testing

--- hello_world.py
+++ hello_world.py
@@ -5,6 +5,7 @@ import usableFunctions
 def helloWorld():
     print("Hello World.")
     usableFunctions.asA()
+    usableFunctions.randomNumber(0, 50)
 
 if __name__ == "__main__":
     helloWorld()
--- usableFunctions.py
+++ usableFunctions.py
@@ -1,2 +1,7 @@
+import random.randint
+
 def asA():
     print("AAA")
+
+def randomNumber(lower_bound, upper_bound):
+    return random.randint(lower_bound, upper_bound) * random.randint(lower_bound, upper_bound) - (0.5 * upper_bound - lower_bound)
